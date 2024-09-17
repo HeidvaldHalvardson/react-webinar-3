040d826f97c856a9ede75d51a83e9de3ca9b4a79
Vlad Bogodyazh <liquid98@yandex.by>
2024-09-17 16:46:17 +0300

    fix list

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Item from '../item';
 import './style.css';
 
-function List({ list, onAddToCart = (code) => {}}) {
+function List({ list, onAddToCart = (_) => {}}) {
   return (
     <div className="List">
       {list.map(item => (
