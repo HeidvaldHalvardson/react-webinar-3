adf8cb91976e4fc6092f19bfd2202893579f0ee1
Vlad Bogodyazh <liquid98@yandex.by>
2024-09-24 16:10:06 +0300

    add/create PagePagination and PagePaginationButton components, create method setPage,task1 done

--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -1,6 +1,8 @@
 import { codeGenerator } from '../../utils';
 import StoreModule from '../module';
 
+const ITEMS_PER_PAGE = 10
+
 class Catalog extends StoreModule {
   constructor(store, name) {
     super(store, name);
@@ -10,19 +12,34 @@ class Catalog extends StoreModule {
   initState() {
     return {
       list: [],
+      currentPage: 1,
+      totalPages: 0,
     };
   }
 
   async load() {
-    const response = await fetch('/api/v1/articles');
+    const skip = (this.getState().currentPage - 1) * ITEMS_PER_PAGE;
+
+    const response = await fetch(`/api/v1/articles?limit=${ITEMS_PER_PAGE}&skip=${skip}&fields=items(_id, title, price),count`);
     const json = await response.json();
+
+    const newTotalPages = Math.ceil(json.result.count / ITEMS_PER_PAGE);
+
     this.setState(
       {
         ...this.getState(),
         list: json.result.items,
+        totalPages: newTotalPages,
       },
       'Загружены товары из АПИ',
     );
+  }
+
+  setPage(page) {
+    this.setState({
+      ...this.getState(),
+      currentPage: page,
+    })
   }
 }
 
